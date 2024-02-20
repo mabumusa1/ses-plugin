cdb6c4f23f7c3e3921e512b39b993a440faf245d
Matic Zagmajster <maticzagmajster@gmail.com>
2024-02-20 21:12:19 +0000

    Use default SES mailer

--- Config/config.php
+++ Config/config.php
@@ -5,28 +5,4 @@ return [
     'description' => 'Adds AWS SES as supported mailer for Mautic.',
     'version'     => '1.0',
     'author'      => 'Steer Campaign',
-    'routes'      => [
-        'main'   => [
-            'plugin_scmailerses_admin' => [
-                'path'       => '/scmailerses/admin',
-                'controller' => 'MauticPlugin\ScMailerSesBundle\Controller\ScMailerSesController::indexAction',
-                'method'     => 'GET',
-            ],
-            'plugin_scmailerses_delete' => [
-                'path'       => '/scmailerses/delete',
-                'controller' => 'MauticPlugin\ScMailerSesBundle\Controller\ScMailerSesController::deleteAction',
-                'method'     => 'POST',
-            ],
-        ],
-    ],
-    'menu'        => [
-        'admin' => [
-            'plugin.scmailerses.admin' => [
-                'route'     => 'plugin_scmailerses_admin',
-                'iconClass' => 'fa-gears',
-                'access'    => 'admin',
-                'priority'  => 60,
-            ],
-        ],
-    ],
 ];
--- Config/services.php
+++ Config/services.php
@@ -14,13 +14,8 @@ return function (ContainerConfigurator $configurator) {
 
     $excludes = [
         'Mailer/Transport/ScSesTransport.php',
-        'Helper/SesHelper.php',
     ];
 
     $services->load('MauticPlugin\\ScMailerSesBundle\\', '../')
         ->exclude('../{'.implode(',', array_merge(MauticCoreExtension::DEFAULT_EXCLUDES, $excludes)).'}');
-
-    $services->set('mailer.transport_factory.sc_ses', \MauticPlugin\ScMailerSesBundle\Mailer\Transport\ScSesFactory::class)
-        ->tag('mailer.transport_factory')
-        ->autowire();
 };
