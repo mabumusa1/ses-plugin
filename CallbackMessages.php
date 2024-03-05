11224dae03b9c0db8906b21f6852d9db84b6634a
Matic Zagmajster <maticzagmajster@gmail.com>
2024-03-05 13:25:40 +0000

    Update messages

--- CallbackMessages.php
+++ CallbackMessages.php
@@ -6,11 +6,11 @@ final class CallbackMessages
 {
     public const INVALID_JSON_PAYLOAD_ERROR = 'MauticAmazonCallback: Invalid JSON Payload.';
 
-    public const TYPE_MISSING_ERROR = 'Key Type not found in payload.';
+    public const TYPE_MISSING_ERROR = 'MauticAmazonCallback: Key Type not found in payload.';
 
-    public const UNSUBSCRIBE_ERROR = 'Callback to SubscribeURL from Amazon SNS failed.';
+    public const UNSUBSCRIBE_ERROR = 'MauticAmazonCallback: SubscribeURL from Amazon SNS failed.';
 
-    public const INVALID_JSON_PAYLOAD_NOTIFICATION_ERROR = 'AmazonCallback: Invalid Notification JSON Payload.';
+    public const INVALID_JSON_PAYLOAD_NOTIFICATION_ERROR = 'MauticAmazonCallback: Invalid Notification JSON Payload.';
 
-    public const UNKNOWN_TYPE_WARNING = 'Type not found in implementaion, no processing was done for type: %s.';
+    public const UNKNOWN_TYPE_WARNING = 'MauticAmazonCallback: Type not found in implementaion, no processing was done for type: %s.';
 }
